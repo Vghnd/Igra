da48dbf940629fdcf99879410bb2d63044842ff2
Vghnd <simonov5554@mail.ru>
2025-04-21 21:40:05 +0300

    Добавлен персонаж и анимация

--- src/scenes/map.ts
+++ src/scenes/map.ts
@@ -1,3 +1,32 @@
-export class Map{
-    
+import mapJSON from '../assets/map.json'
+import { TILES,SIZES, LAYERS, SPRITES } from '../utils/constants';
+import {Player} from '../entities/player'
+export class Map extends Phaser.Scene{
+    private player?: Player;
+    constructor(){
+        super('MapScene');
+    }
+    preload(){
+        this.load.image(TILES.MAP,'src/assets/Winter_Tiles.png')
+        this.load.tilemapTiledJSON('map1','src/assets/map.json')
+        this.load.spritesheet(SPRITES.PLAYER,"src/assets/characters/alliance.png",{
+            frameWidth:SIZES.Player.WIDTH,
+            frameHeight:SIZES.Player.HEIGHT
+        })
+    }
+    create(){
+        const map = this.make.tilemap({key: "map1"});
+        const tileset = map.addTilesetImage(mapJSON.tilesets[0].name,TILES.MAP,SIZES.TILE,SIZES.TILE);
+        const ledLayer = map.createLayer(LAYERS.LED,tileset,0,0);
+        const wallsLayer = map.createLayer(LAYERS.WALLS,tileset,0,0);
+
+
+        this.player = new Player(this,400,250,SPRITES.PLAYER)
+    }
+
+
+    update(time: number, delta: number): void {
+        this.player.update(delta);
+    }
 }
+
---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+export const TILES = {
+    MAP: 'map'
+}
+
+export const SIZES = {
+    TILE:32,
+    Player:{
+        WIDTH:48,
+        HEIGHT:48
+    }
+}
+
+export const LAYERS = {
+    LED: 'led',
+    WALLS: 'walls'
+}
+
+export const SPRITES = {
+    PLAYER: 'Player'
+}
